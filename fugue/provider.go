15f060f764bdc80ec1280ca02f021a2646784bba
Evan Nemerson <evan.nemerson@fugue.co>
2021-09-16 09:44:55 -0400

    Add notification resource (NOTE: create is currenly broken).
    This currently does not work as the server returns unexpecetd values
    when creating a notification.  Specifically, the created_at value is
    a float insead of an integer, and the environments are a map of
    UUID->name instead of an array of maps of UUID->name.  Because of those
    issues this resource is largely untested and will need to be revisied
    after they are resolved.

--- fugue/provider.go
+++ fugue/provider.go
@@ -32,6 +32,7 @@ func Provider() *schema.Provider {
 			"fugue_rule_waiver":        resourceRuleWaiver(),
 			"fugue_rule":               resourceRule(),
 			"fugue_family":             resourceFamily(),
+			"fugue_notification":       resourceNotification(),
 		},
 		DataSourcesMap: map[string]*schema.Resource{
 			"fugue_aws_types":    dataSourceAwsTypes(),
